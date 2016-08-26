27f1243dcc231de9534241c876387b06a7d04d8d
Jörn Röder <kontakt@joernroeder.de>
2016-08-26 15:53:47 +0200

    pointed COpenSSL to joernroeder/COpenSSL

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "OpenSSL",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/joernroeder/COpenSSL.git", majorVersion: 0, minor: 6),
     ]
 )
